af2e113fc89479467a36c44e09e79b675c67c87f
shane ryan <onyx.shane@gmail.com>
2017-10-27 11:16:57 -0700

    improve front end logic hopefully

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-//back end
+// //back end
+var userString = function(number){
+  for (var i = 0; i < number; i++)
+  var index = result.indexOf(i);
+  result.push(i);
+};
 
 
 //front end
 $(document).ready (function() {
   $('#input').submit(function(event){
     event.preventDefault();
+    $('#output').empty();
     var userNumber = parseInt($('#user-number').val());
-    $(#output).text(userString(userNumber));
+    var output = countUp(userNumber);
+
+    output.forEach(function(_numberList){
+      $('#output').append('<li>' + _numberList + '</li>')
+    });
   });
 });
