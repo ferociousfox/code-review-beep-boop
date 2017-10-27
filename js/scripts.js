a4b7d11647f17ce9c110b6f7992dc5dcd63e18ff
shane ryan <onyx.shane@gmail.com>
2017-10-27 16:03:07 -0700

    add back end logic to have the page function with input of 0-9

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,21 @@ var userString = function(userNumber){
   var userArray = [];
   for (var i = 0; i <= userNumber; i++) {
    userArray.push(i);
-   if(i % 3 === 0){
-     userArray[i] = "I'm sorry Dave, I cannot do that"
-   };
+   if (i === 0){
+     userArray[i] = " beep! "
+   } else if (i === 1){
+    userArray[i] = " boop! "
+   } else if (i % 3 === 0){
+    userArray[i] = " I'm sorry dave, I can't do that. "
   };
-  return userArray;
+
+
+
+
 };
+return userArray;
+};
+
 
 
 
@@ -18,7 +27,8 @@ var userString = function(userNumber){
 $(document).ready (function() {
   $('#hal9000').submit(function(event) {
     event.preventDefault();
+    $('#output').empty();
     var userNumber = parseInt($('#user-number').val());
-    $('#output').text(userString(userNumber));
+    $('#output').append(userString(userNumber));
   });
 });
